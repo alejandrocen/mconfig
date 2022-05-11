ddf350e3ab1dde1c2356240e6acb53f2a9925d17
Alejandro Cen <alex96.cen@gmail.com>
2022-05-11 00:57:56 -0500

    Use rails root for proto files

--- config/initializers/proto.rb
+++ config/initializers/proto.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-proto_dir = File.join(__dir__, '../../proto')
+proto_dir = Rails.root.join('proto').to_s
 $LOAD_PATH.unshift(proto_dir) unless $LOAD_PATH.include?(proto_dir)
 
 require 'mconfig_services_pb'
